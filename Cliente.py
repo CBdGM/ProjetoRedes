8f80293864a65fa38a5a16bb90342d98420d317d
CBdGM <112808999+CBdGM@users.noreply.github.com>
2022-10-10 11:00:43 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,5 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import random
+import cryptocode
 
 
 def Handshake(mClientSocket, A_ChavePrivClient):
@@ -47,13 +48,27 @@ def Handshake(mClientSocket, A_ChavePrivClient):
             print(f"cipher servidor: {Y_cipherServidor}")
         
 
+
         if req == "HANDSHAKE FINISHED":
             chave_secreta_cliente = int(pow(Y_cipherServidor, A_ChavePrivClient, P_ChavePubServ)) #calculo da chave secreta
             print(f"Chave secreta cliente: {chave_secreta_cliente}\n")
-
             mClientSocket.send(req.encode()) #alertanado para o servidor que ja possui a chave secreta
             manter_conexao = False #finalizamos o handshake e ja temos a chave secreta que vai ser usada na criptografia
-            return chave_secreta_cliente
+            return str(chave_secreta_cliente)
+
+#função que lida com as requisições get
+def GET(mClientSocket, req):
+
+    req = cryptocode.encrypt(req, chave_secreta_cliente) #criptografia da requisição
+    req = req.encode()
+    mClientSocket.send(req) # envio da requisição
+
+    dados = mClientSocket.recv(2048) # recebimento dos dados respondidos pelo servidor após a requisição
+    dados = dados.decode()
+    dados = cryptocode.decrypt(dados, chave_secreta_cliente) # descriptografia
+    print(dados)
+
+
 
 
 #DADOS
@@ -62,7 +77,6 @@ def Handshake(mClientSocket, A_ChavePrivClient):
 # a função randint escolher um numero aleatório. (1, 64) diz que esse numero vai ser entre 1 e 64
 # eu so escolhi qualquer numero para ser o 64, podia ser qualquer um (acho)
 A_ChavePrivClient = random.randint(1, 64) 
-
 
 
 mClientSocket = socket(AF_INET, SOCK_STREAM) #criando o socket
@@ -74,8 +88,5 @@ chave_secreta_cliente = Handshake(mClientSocket, A_ChavePrivClient)
 
 # essa é a primeira requisição feita para o servidor
 # apenas um get de teste que recebe uma string com conteudo html
-req = "GET teste"
-mClientSocket.send(req.encode())
-
-dados = mClientSocket.recv(2048)
-print(dados.decode())
+req = "teste"
+GET(mClientSocket, req)
